ffa82378346ea82bde4bfd4d3c4000ce704b8749
ShuheiHanadate <hanadate@ShuheiHandatenoMacBook-Air.local>
2017-08-27 23:57:42 +0900

    add udemy

--- practice/scr/extract_cond_cart.R
+++ practice/scr/extract_cond_cart.R
@@ -4,19 +4,22 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 
-tmp <- data.frame(Titanic)
-df <- data.frame(
-  Class = rep(tmp$Class, tmp$Freq),
-  Sex = rep(tmp$Sex, tmp$Freq),
-  Age = rep(tmp$Age, tmp$Freq),
-  Survived = rep(tmp$Survived, tmp$Freq)
-)
-ct <- rpart(Survived ~ Class + Sex + Age, data = df, method = "class")
-ct.party <- as.party(ct)
+# mtcarsを読込確認
+tmp <- data.frame(mtcars)
 
-cond <- partykit:::.list.rules.party(ct.party, i = nodeids(ct.party))
+# シリンダー数,V/S,オートマ・マニュアル,ギア数,キャブ数をファクター化
+df <- tmp %>% 
+  mutate_each_(funs(as.factor), list("cyl","vs","am","gear","carb"))
+
+# 燃費を目的変数にして回帰木生成
+ct <- rpart(mpg ~ ., data = df, method = "anova")
+(ct.party <- as.party(ct))
+
+# 条件をベクトルで抜き出し
+(cond <- partykit:::.list.rules.party(ct.party, i = nodeids(ct.party)))
+
+# 決定木分岐条件一覧表に整形
 cond.df <- data.frame(INDEX=1:length(cond), CONDITIONS=cond)
-
 cond.df.split <- cond.df %>%
   tidyr::separate(col=CONDITIONS, 
                   into=paste("CONDITIONS",as.character(c(1:(1+max(str_count(.$CONDITIONS,(" & ")))))),sep=""), 
@@ -37,5 +40,6 @@ cond.df.split <- cond.df %>%
   dplyr::summarise(rVALUE_min=min(rVALUE), lVALUE_max=max(lVALUE)) %>% 
   dplyr::ungroup(.) %>% 
   dplyr::mutate(VALUE= str_c(lVALUE_max, rVALUE_min, sep="~")) %>% 
-  dplyr::mutate(VALUE= str_replace_all(VALUE, c("-Inf"="","Inf"="","c[(]"="","[)]~"=""))) %>% 
+  dplyr::mutate(VALUE= str_replace_all(VALUE, c("-Inf"="","Inf"="","c[(]"="","[)]~"="","\""=""))) %>% 
   dplyr::select(INDEX, CONDITIONS, VALUE)
+(cond.df.split)
