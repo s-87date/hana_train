21d64c49d742ab37d6800b5707120364e5820d77
HANADATE <hanadate@jp.ibm.com>
2018-08-06 19:09:12 +0900

    Create tubepay

---
+++ practice/scr/tubeplay.R
@@ -0,0 +1,20 @@
+# install.packages("miniUI")
+library(utils)
+library(miniUI)
+tubeplay <- function(url=NA, 
+                     width = "560", height = "315", background = "white",
+                     viewer = getOption("viewer", utils::browseURL)) {
+  if(is.na(url)==TRUE){
+    print("ENTER URL")
+  }
+  # make video_id
+  video_id <- gsub("^.*\\?v=", "", url)
+  # make_ui
+  ui <- miniUI::miniPage(
+    htmltools::tags$iframe(width = width, height = height,
+                           src = paste("https://www.youtube.com/embed", video_id, sep = "/"),
+                           frameborder="0")
+  )
+  htmltools::html_print(ui)
+}
+tubeplay("https://www.youtube.com/watch?v=rY7Cw4YAF3c")
